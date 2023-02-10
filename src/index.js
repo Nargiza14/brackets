dd1256f25816f48474394f67a88a045be34d5a16
Nargiza14 <ruzieva.aya@icloud.com>
2023-02-10 12:29:56 +0600

    update: change index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.join('').replace(/,/g, '');
+    const given = [];
+  for (let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+    if (bracketsIndex % 2 === 0) {
+      if (bracket === brackets[bracketsIndex + 1] && given[given.length - 1] ===  bracketsIndex){
+        given.pop();
+      } else if (bracket === brackets[bracketsIndex + 1] && given[given.length - 1] !== bracketsIndex) {
+        given.push(bracketsIndex)
+      }
+      else{
+        given.push(bracketsIndex)
+      }
+    }
+    else {
+      if (given.pop() !== bracketsIndex-1){
+        return false;
+      }
+    }
+  }
+  return given.length === 0
 }
